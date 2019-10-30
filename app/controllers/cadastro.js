94f2280991bf76bf064537b518c7ea0e577578da
Grupo5-Reculos Gerbi Neto <netogerbi2@hotmail.com>
2019-10-30 20:05:37 -0300

    Creating cadastro form validation

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -1,3 +1,22 @@
 module.exports.cadastro = function(app, req, res) {
-  res.render('cadastro')
+  res.render('cadastro', { invalid: {}, formData: {} } )
+}
+
+module.exports.cadastrar = function(app, req, res) {
+
+  const formData = req.body
+  console.log(formData)
+
+  req.assert('nome', 'Nome não pode ser vazio').notEmpty()
+  req.assert('usuario', 'Usuário não pode ser vazio').notEmpty()
+  req.assert('senha', 'Senha não pode ser vazio').notEmpty()
+  req.assert('casa', 'Casa não pode ser vazio').notEmpty()
+
+  const errors = req.validationErrors()
+  if(errors) {
+    res.render('cadastro' , { invalid: errors, formData: formData });
+    return;
+  }
+
+  res.send('cadastro ok')
 }
